cf8607db2941be30a58c748c6be67996c3f8d675
rohan  desai <48354186+desairohan@users.noreply.github.com>
2022-11-30 19:33:50 +0530

    First commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
   root: true,
+  // env: {
+  //   node: true,
+  // },
   env: {
-    node: true,
+    es6: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
   parserOptions: {
-    parser: 'babel-eslint',
+    ecmaVersion: "latest",
+  },
+  extends: ["plugin:vue/vue3-essential", "@vue/airbnb"],
+  parserOptions: {
+    parser: "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
